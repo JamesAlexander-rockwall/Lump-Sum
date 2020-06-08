251d9244c4039e7e697aef2f55fe60344c342576
James Alexander <james.alexander.rockwall@gmail.com>
2020-06-08 10:54:05 -0700

    First result

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/lumpsum';
-// $config['base_url'] = 'https://lumpsum.herokuapp.com/';
+//  $config['base_url'] = 'http://localhost/lumpsum';
+    $config['base_url'] = 'https://lumpsum.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
